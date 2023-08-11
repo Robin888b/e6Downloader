a2f4187d88e43d80fb7b82bd66c7523a4dd4448e
Raven Spart • Robin888b <83099065+Robin888b@users.noreply.github.com>
2023-08-12 01:09:46 +0200

    Add code x)

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace e621Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.QueryTextBox = new System.Windows.Forms.TextBox();
+            this.fileTextbox = new System.Windows.Forms.TextBox();
+            this.folderTextBox = new System.Windows.Forms.TextBox();
+            this.pageTextBox = new System.Windows.Forms.TextBox();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFolderBtn = new System.Windows.Forms.Button();
+            this.DownloadBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // QueryTextBox
+            // 
+            this.QueryTextBox.Location = new System.Drawing.Point(12, 25);
+            this.QueryTextBox.Name = "QueryTextBox";
+            this.QueryTextBox.Size = new System.Drawing.Size(259, 20);
+            this.QueryTextBox.TabIndex = 0;
+            this.QueryTextBox.Text = "Gay wolf fox order:score rating:e";
+            // 
+            // fileTextbox
+            // 
+            this.fileTextbox.Location = new System.Drawing.Point(12, 70);
+            this.fileTextbox.Name = "fileTextbox";
+            this.fileTextbox.Size = new System.Drawing.Size(259, 20);
+            this.fileTextbox.TabIndex = 1;
+            this.fileTextbox.Text = "Gay fox wolf";
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Location = new System.Drawing.Point(12, 166);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(224, 20);
+            this.folderTextBox.TabIndex = 2;
+            // 
+            // pageTextBox
+            // 
+            this.pageTextBox.Location = new System.Drawing.Point(12, 118);
+            this.pageTextBox.MaxLength = 4;
+            this.pageTextBox.Name = "pageTextBox";
+            this.pageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pageTextBox.TabIndex = 3;
+            this.pageTextBox.Text = "99";
+            this.pageTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(118, 118);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(153, 20);
+            this.usernameTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Query";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Folder";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Pages to download";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(118, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Your UserName";
+            // 
+            // openFolderBtn
+            // 
+            this.openFolderBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openFolderBtn.ForeColor = System.Drawing.Color.Black;
+            this.openFolderBtn.Location = new System.Drawing.Point(242, 164);
+            this.openFolderBtn.Name = "openFolderBtn";
+            this.openFolderBtn.Size = new System.Drawing.Size(29, 23);
+            this.openFolderBtn.TabIndex = 10;
+            this.openFolderBtn.Text = "...";
+            this.openFolderBtn.UseVisualStyleBackColor = true;
+            // 
+            // DownloadBtn
+            // 
+            this.DownloadBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownloadBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownloadBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(129)))), ((int)(((byte)(224)))));
+            this.DownloadBtn.Location = new System.Drawing.Point(12, 206);
+            this.DownloadBtn.Name = "DownloadBtn";
+            this.DownloadBtn.Size = new System.Drawing.Size(259, 28);
+            this.DownloadBtn.TabIndex = 11;
+            this.DownloadBtn.Text = "Start Download";
+            this.DownloadBtn.UseVisualStyleBackColor = true;
+            this.DownloadBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(35)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(288, 246);
+            this.Controls.Add(this.DownloadBtn);
+            this.Controls.Add(this.openFolderBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.pageTextBox);
+            this.Controls.Add(this.folderTextBox);
+            this.Controls.Add(this.fileTextbox);
+            this.Controls.Add(this.QueryTextBox);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(304, 285);
+            this.MinimumSize = new System.Drawing.Size(304, 285);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "E621 Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox QueryTextBox;
+        private System.Windows.Forms.TextBox fileTextbox;
+        private System.Windows.Forms.TextBox folderTextBox;
+        private System.Windows.Forms.TextBox pageTextBox;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button openFolderBtn;
+        private System.Windows.Forms.Button DownloadBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace e621Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = QueryTextBox.Text;
+            int pages = Int16.Parse(pageTextBox.Text);
+            string fileName = fileTextbox.Text;
+            string folderName = folderTextBox.Text;
+            string user = usernameTextBox.Text;
+            bool useMultipleFolder = true;
+
+            if (!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), folderName)))
+            {
+                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), folderName));
+            }
+
+            string folder = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+            if (useMultipleFolder)
+            {
+                CreateSubfolderIfNotExists(folder, "rating_s");
+                CreateSubfolderIfNotExists(folder, "rating_q");
+                CreateSubfolderIfNotExists(folder, "rating_e");
+            }
+
+            string url = "https://e621.net/posts.json?tags=" + query;
+            string userAgent = "e621_downloader.cs";
+            int u = 1;
+
+            while (u <= pages)
+            {
+                string finalUrl = url + "&page=" + u;
+                Console.WriteLine($"\n\nPage N°{u}\n");
+
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers.Add("User-Agent", userAgent);
+                    client.Headers.Add("From", user);
+                    string resultJson = client.DownloadString(finalUrl);
+                    JObject resultObject = JObject.Parse(resultJson);
+
+                    JArray posts = (JArray)resultObject["posts"];
+
+                    foreach (JToken post in posts)
+                    {
+                        string imageUrl = post["file"]["url"].ToString();
+
+                        if (!string.IsNullOrEmpty(imageUrl))
+                        {
+                            string file;
+                            if (useMultipleFolder)
+                            {
+                                file = Path.Combine(folder, "rating_" + post["rating"].ToString(), fileName + post["id"] + "." + post["file"]["ext"]);
+                            }
+                            else
+                            {
+                                file = Path.Combine(folder, fileName + post["id"] + "." + post["file"]["ext"]);
+                            }
+
+                            using (WebClient downloadClient = new WebClient())
+                            {
+                                using (Stream stream = downloadClient.OpenRead(imageUrl))
+                                {
+                                    using (FileStream fileStream = File.Create(file))
+                                    {
+                                        stream.CopyTo(fileStream);
+                                    }
+                                }
+                            }
+
+                            Console.WriteLine("Image successfully Downloaded: " + file);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Image Couldn't be retrieved");
+                        }
+                    }
+                }
+
+                u++;
+            }
+        }
+        static void CreateSubfolderIfNotExists(string parentFolder, string subfolder)
+        {
+            string subfolderPath = Path.Combine(parentFolder, subfolder);
+            if (!Directory.Exists(subfolderPath))
+            {
+                Directory.CreateDirectory(subfolderPath);
+            }
+        }
+    }
+}
